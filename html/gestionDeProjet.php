b7c5173d717450e3c6ff6e879831d2343d5ee16e
Erwan GUEGANIC <GUEGANIC_Er@lprs.local>
2024-03-15 15:37:37 +0100

    fix : sidebar

--- html/gestionDeProjet.php
+++ html/gestionDeProjet.php
@@ -5,17 +5,107 @@
   <title>ProjetStock</title>
   <link rel="stylesheet" href="../css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <style>
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
+
+    .nav-button {
+      position: relative;
+      margin-left: 16px;
+      padding-left: 16px;
+      height: 54px;
+      display: flex;
+      align-items: center;
+      color: var(--navbar-light-secondary);
+      direction: ltr;
+      cursor: pointer;
+      z-index: 1;
+      transition: color 0.2s, padding-left 0.2s;
+    }
+
+    .nav-button:hover {
+      padding-left: 24px;
+    }
+
+    .nav-button span {
+      transition: transform 0.2s;
+    }
+
+    .nav-button:hover span {
+      transform: translateX(5px);
+    }
+
+    .container {
+      max-width: 800px;
+      margin: 20px auto;
+      padding: 20px;
+      background-color: #f8f8f8;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      border-radius: 10px;
+      position: relative;
+      flex-grow: 1;
+    }
+
+    .checkbox-container {
+      margin-bottom: 20px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
+
+    .box {
+      border: 1px solid #ccc;
+      border-radius: 10px;
+      padding: 20px;
+      margin-bottom: 20px;
+      width: calc(33.33% - 20px);
+      box-sizing: border-box;
+      background-color: #f0f0f0;
+    }
+
+    .box:nth-child(3n) {
+      margin-right: 0;
+    }
+
+    .box:last-child {
+      margin-right: 0;
+    }
+
+    .button-container {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 20px;
+    }
+
+    .button-container button {
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    .button-container button.modify {
+      background-color: #3498db;
+      color: #fff;
+    }
+
+    .button-container button.delete {
+      background-color: #e74c3c;
+      color: #fff;
+    }
+
+    .dimension-label {
+      font-size: 14px;
+      margin-bottom: 5px;
+    }
+
+  </style>
 </head>
-<style>
-  body{
-    font-family: 'Arial', sans-serif;
-    background-color: #f4f4f4;
-    margin: 0;
-    padding: 0;
-  }
-</style>
 <body>
-</style>
 <div id="nav-bar">
   <input id="nav-toggle" type="checkbox"/>
   <div id="nav-header">
@@ -46,17 +136,5 @@
     </div>
   </div>
 </div>
-
-<!-- Script jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  $(document).ready(function() {
-    
-    $('#deconnexion').on('click', function() {
-      window.location.href = "../src/php/utilisateur/deconnexion.php"; 
-    });
-  });
-</script>
-
 </body>
 </html>
