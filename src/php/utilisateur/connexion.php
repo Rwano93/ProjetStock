5c0222c8840bc45e8b9027782ae8123e0a1dc403
Erwan GUEGANIC <GUEGANIC_Er@lprs.local>
2024-05-03 08:24:46 +0200

    fix: connexion

--- src/php/utilisateur/connexion.php
+++ src/php/utilisateur/connexion.php
@@ -3,17 +3,19 @@
 var_dump($_POST);
 
 include 'Utilisateur.php';
-
+require_once '../../bdd/SQLConnexion.php';
 $email = $_POST['email'];
 $mdp = $_POST['mdp'];
 
-$estOk = Utilisateur::CONNEXION($email, $mdp);
+$estOk = new Utilisateur(
+    [
+        'email' => $email,
+        'mdp' => $mdp
+    ]
+);
+$estOk->CONNEXION($email, $mdp);
 if ($estOk){
     header('Location: ../../../html/gestionMatiere.php');
 }else{
     header('Location: ../../../html/connexion.html');
 }
-
- 
-
-
