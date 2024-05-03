a577d902ae230c0ac969a05ad15d07812cd3bf68
Erwan GUEGANIC <GUEGANIC_Er@lprs.local>
2024-05-03 09:20:59 +0200

    fix: matiere

--- src/matiere/addMatiere.php
+++ src/matiere/addMatiere.php
@@ -1,9 +1,36 @@
 <?php
-include '..//bdd/projetstock.sql';
-include 'matiere.php';
+include '../bdd/SQLConnexion.php';
+include '../matiere/Matiere.php';
 
-$matiere = new Matiere($_POST);
+$nom = $_POST['nom'];
+$longueur = $_POST['longueur'];
+$largeur = $_POST['largeur'];
+$hauteur = $_POST['hauteur'];
+$epaisseur = $_POST['epaisseur'];
+$diametre = $_POST['diametre'];
+
+
+$matiere = new Matiere([
+  'nom' => $nom,
+  'longueur' => $longueur,
+  'largeur' => $largeur,
+  'hauteur' => $hauteur,
+  'epaisseur' => $epaisseur,
+  'diametre' => $diametre
+]);
+
+
 $matiere->ajouter();
 
-?>
 
+echo '<div class="box">
+        <div class="dimension-label">' . $nom . '</div>
+        <div class="dimension-label">Longueur: ' . $longueur . '</div>
+        <div class="dimension-label">Largeur: ' . $largeur . '</div>
+        <div class="dimension-label">Hauteur: ' . $hauteur . '</div>
+        <div class="button-container">
+          <button class="modify">Modifier</button>
+          <button class="delete">Supprimer</button>
+        </div>
+      </div>';
+?>
